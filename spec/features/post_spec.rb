edce980587fcb06e4fcd16b39783fd1ef9374c51
Hannah M <hannahemalm616@gmail.com>
2021-04-01 16:08:46 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -9,5 +9,11 @@ describe 'navigate' do
   it 'loads the show page' do
     visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
+    
+     
+  it 'shows the title on the show page in an h1 tag' do
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("h1", text: "My Post")
+
   end
 end
